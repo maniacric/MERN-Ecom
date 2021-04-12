36e1a38a46442abe1c94ec25b45be37b7e04ee88
maniacric <iamrishabhpal@gmail.com>
2021-04-12 14:39:22 +0530

    view product route created

--- proj_backend/routes/product.js
+++ proj_backend/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const {isSignedin,isAuthenticated,isAdmin} = require("../controllers/auth");
-const {getProductbyId,createProduct} = require("../controllers/product");
+const {getProductbyId,createProduct,getProduct, photo} = require("../controllers/product");
 const {getUserbyId} = require("../controllers/user");
 
 //param
@@ -12,7 +12,8 @@ router.param("productId",getProductbyId);
 
 //All actual route
 router.post("/product/create/:userId",isSignedin,isAuthenticated,isAdmin,createProduct)
-
+router.get("/product/:productId",getProduct);
+router.get("/product/photo/:productId",photo);
 
 
 module.exports = router;
