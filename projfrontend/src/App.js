574c23efd97b8615fb85e9a99b00b51bbffbc7d9
maniacric <iamrishabhpal@gmail.com>
2021-04-18 01:04:49 +0530

    frontend begins

---
+++ projfrontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+
+export default function App (){
+  return (
+    <div>
+      <h1>
+        Hi
+      </h1>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {API} from "../backend" 
+
+
+export default function Home(){
+    console.log("API is",API)
+    return (
+        <div>
+            <h1>
+                Hello frontEnd
+            </h1>
+        </div>
+    )
+}
+
