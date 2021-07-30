085f1f403563a07fb97fac5fdcafe8359c6771c3
maniacric <iamrishabhpal@gmail.com>
2021-07-31 00:05:37 +0530

    basic form

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,10 +1,35 @@
 import React,{useState} from 'react'
 import Base from '../core/Base'
 
+const signinForm = () => (
+    <div className = "row">
+        <div className = "col-md-6 offset-sm-3 text-left">
+            <form>
+                <div className ="form-group">
+                    <label className ="text-light">Name</label>
+                    <input className = "form-control" type = "text"></input>
+                </div>
+                <div className ="form-group">
+                    <label className ="text-light">Email</label>
+                    <input className = "form-control" type = "email"></input>
+                </div>
+                <div className ="form-group">
+                    <label className ="text-light">Password</label>
+                    <input className = "form-control" type = "text"></input>
+                </div>
+                <button className = "btn btn-success btn-blk">Submitt</button>
+            </form>
+        </div>
+    </div>
+)
+
+
+
 const Signin = ()=> {
     return (
         <div>
             <Base>
+            {signinForm()}
                 Signin page
             </Base>
         </div>
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,11 +1,34 @@
 import React,{useState} from 'react'
 import Base from '../core/Base'
 
+
+const signUpForm = () => (
+    <div className = "row">
+        <div className = "col-md-6 offset-sm-3 text-left">
+            <form>
+                <div className ="form-group">
+                    <label className ="text-light">Name</label>
+                    <input className = "form-control" type = "text"></input>
+                </div>
+                <div className ="form-group">
+                    <label className ="text-light">Email</label>
+                    <input className = "form-control" type = "email"></input>
+                </div>
+                <div className ="form-group">
+                    <label className ="text-light">Password</label>
+                    <input className = "form-control" type = "text"></input>
+                </div>
+                <button className = "btn btn-success btn-blk">Submitt</button>
+            </form>
+        </div>
+    </div>
+)
+
 const Signup = ()=> {
     return (
         <div>
-            <Base>
-                Signup page
+            <Base>  
+                {signUpForm()}
             </Base>
         </div>
     )
