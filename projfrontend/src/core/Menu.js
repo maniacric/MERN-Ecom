319248d13dcb045dde83bf99844e2f6747ba2d2d
maniacric <iamrishabhpal@gmail.com>
2021-07-30 23:38:59 +0530

    added some styling on menu

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -2,38 +2,48 @@ import { bigIntLiteral } from '@babel/types'
 import React from 'react'
 import {Link,withRouter} from "react-router-dom"
 
+const currentTab = (history,path) =>{
+    if(history.location.pathname == path )
+        return {color: "#FFFFFF"}
+    return {color : "D1D1D1"}
+}  
 
-const Menu = () => (
+const Menu = ({history}) => (
 
     <div>
         <ul className = "nav nav-tabs bg-dark">
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/")}className = "nav-link" to ="/">
                     Home
                 </Link>
             </li>
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/signup")} className = "nav-link" to ="/signup">
                     Signup
                 </Link>
             </li>
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/signin")} className = "nav-link" to ="/signin">
                     Signin
                 </Link>
             </li>
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/signout")} className = "nav-link" to ="/signout">
                     Signout
                 </Link>
             </li>
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/user/dashboard")} className = "nav-link" to ="/user/dashboard">
                     A.Dashboard
                 </Link>
             </li>
             <li className = "nav-item">
-                <Link className = "nav-link" to ="/">
+                <Link style = {currentTab (history,"/admin/dashboard")} className = "nav-link" to ="/admin/dashboard">
+                    A.Dashboard
+                </Link>
+            </li>
+            <li className = "nav-item">
+                <Link style = {currentTab (history,"/cart")} className = "nav-link" to ="/cart">
                     cart
                 </Link>
             </li>
