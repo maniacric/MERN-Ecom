74a364c0e4ea7434eda3a83ff7b453d3a4e17cec
maniacric <iamrishabhpal@gmail.com>
2021-08-03 23:58:34 +0530

    basic template done left admin

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { isAuthenticated } from '../auth/helper'
 import Base from '../core/Base'
+import { Link } from 'react-router-dom'
 
 const AdminDashBoard= () =>{
 
@@ -9,9 +10,37 @@ const AdminDashBoard= () =>{
     const adminLeftSide = () =>{
         return (
             <div className = "card">
-                <h4 className ="card-header bg-dark text-white">
+                <div className ="card-header bg-success text-white text-center">
                     Admin Navigation
-                </h4>
+                    <ul className= "list-group-item">
+                        <li>
+                        <Link to ="/admin/create/category"className = "nav-link text-info">
+                            Create Category
+                        </Link>
+                        </li>
+                    </ul>
+                    <ul className= "list-group-item">
+                        <li>
+                        <Link to ="/admin/category"className = "nav-link text-info">
+                            Manage Category
+                        </Link>
+                        </li>
+                    </ul>
+                    <ul className= "list-group-item">
+                        <li>
+                        <Link to ="/admin/create/product"className = "nav-link text-info">
+                            Create Products
+                        </Link>
+                        </li>
+                    </ul>
+                    <ul className= "list-group-item">
+                        <li>
+                        <Link to ="/admin/product"className = "nav-link text-info">
+                            Manage Products
+                        </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
@@ -19,27 +48,30 @@ const AdminDashBoard= () =>{
     const adminRightSide = () =>{
         return (
              <div className = "card">
-                <h4 className ="card-header bg-dark text-white">
+                <h4 className ="card-header bg-success text-white">
                     Admin Navigation
+                   
+
                 </h4>
             </div>
         )
     }
 
     return (
-    <div>
-        <Base title ="Admin Dashboard">
+        <Base title ="Welcome to Admin Dashboard"
+            description  ="Manage all Products"
+            className ="container bg-info p-5-4"
+        >
             <div className = "row">
                 <div className ="col-3">   
                     {adminLeftSide()}
                 </div>
                 <div className ="col-9"> 
-                    {adminRightSide()}
+                    
                 </div>
             </div>
         </Base>
          
-    </div>
     )
 }
 
