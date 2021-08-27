bf4d555bc04638fb45a4873110d84e2f0e60e8a4
maniacric <iamrishabhpal@gmail.com>
2021-08-27 23:16:32 +0530

    order created

--- projfrontend/src/user/UserDashBoard.js
+++ projfrontend/src/user/UserDashBoard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Base from '../core/Base'
+
 import { isAuthenticated } from '../auth/helper'
 
-const {user: {name,email,role}} =  isAuthenticated();
 
 
 const UserDashBoard= () =>{
